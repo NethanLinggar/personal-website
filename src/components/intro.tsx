36b60f20f235aa5edc7bab8a01c568094507cfbf
Nethan Linggar <nethan.linggar@gmail.com>
2023-09-14 23:12:02 +0700

    feat: added intro section

---
+++ src/components/intro.tsx
@@ -0,0 +1,101 @@
+"use client";
+
+import Image from 'next/image';
+import React from 'react';
+import { motion } from 'framer-motion';
+import Link from 'next/link';
+import { BsArrowRight, BsLinkedin } from 'react-icons/bs';
+import { HiDownload } from 'react-icons/hi';
+import { FaGithubSquare } from 'react-icons/fa';
+
+export default function Intro() {
+  return (
+    <section className='mb-28 max-w-[50rem] text-center sm:mb-0'>
+      <div className='flex items-center justify-center'>
+        <div className='relative'>
+          <motion.div
+            initial={{ opacity: 0, scale: 0}}
+            animate={{ opacity: 1, scale: 1}}
+            transition={{
+              type: "tween",
+              duration: 0.2,
+            }}
+          >
+            <Image 
+              src='https://media.licdn.com/dms/image/D5603AQFOMopF2JVMJg/profile-displayphoto-shrink_800_800/0/1693996109582?e=1700092800&v=beta&t=7tD33BQj4OqXnzKvmcyb6efVcsAbGv5I26xmS8NYGkY' 
+              alt='Nethan Portrait' 
+              width='192'
+              height='192'
+              quality='95'
+              priority={true}
+              className='h-24 w-24 rounded-full object-cover border-[0.35rem] border-white shadow-xl'
+            />
+          </motion.div>
+          
+          <motion.span className='absolute text-2xl bottom-0 right-0'
+            initial={{ opacity: 0, scale: 0}}
+            animate={{ opacity: 1, scale: 1}}
+            transition={{
+              type: "spring",
+              stiffness: "125",
+              delay: 0.1,
+              duration: 0.7,
+            }}
+          >
+            👋
+          </motion.span>
+        </div>
+      </div>
+
+      <motion.h1
+        className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl"
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+      >
+        <span className="font-bold">Hey there, I'm Nethan!</span> A{" "}
+        <span className="font-bold">computer science undergraduate</span> that's interested in {" "}
+        <span className="italic">Software Engineering</span> and <span className="italic">Machine Learning</span>. Welcome to my{" "}
+        <span className="underline">website</span>!
+      </motion.h1>
+
+      <motion.div
+        className='flex flex-col sm:flex-row items-center justify-center gap-3 px-4 text-lg font-medium'
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0}}
+        transition={{
+          delay: 0.1
+        }}
+      >
+        <Link 
+          href='#contact'
+          className='group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition'
+        >
+          Contact me here{" "}
+          <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition"/>
+        </Link>
+        <a
+          className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer border-black/10"
+          href="/CV_Nethaneel_Patricio_Linggar.pdf"
+          download
+        >
+          Download CV{" "}
+          <HiDownload className="opacity-70 pb-0.5 group-hover:translate-y-0.5 transition" />
+        </a>
+        <a
+          className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer border-black/10"
+          href="https://www.linkedin.com/in/nethaneel-patricio-linggar/"
+          target="_blank"
+        >
+          <BsLinkedin />
+        </a>
+        <a
+          className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer border-black/10"
+          href="https://github.com/NethanLinggar"
+          target="_blank"
+        >
+          <FaGithubSquare />
+        </a>
+      </motion.div>
+    </section>
+  )
+}
