de43a1f29cf4b2cd91f287e8f27a6a6908e88546
Nethan Linggar <nethan.linggar@gmail.com>
2023-10-02 20:00:22 +0700

    fix: resolve project layout and update data

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -58,25 +58,18 @@ export const experiencesData = [
 
 export const projectsData = [
   {
-    title: "CorpComment",
+    title: "ERP (Forecasting Module)",
     description:
-      "I worked as a full-stack developer on this startup project for 2 years. Users can give public feedback to companies.",
-    tags: ["React", "Next.js", "MongoDB", "Tailwind", "Prisma"],
+      "I worked as a full-stack developer on this project. Users can instantly make demand forecasts based on their inventory.",
+    tags: ["Next.js", "Tailwind", "Express.js", "MySQL",  "Python"],
     imageUrl: corpcommentImg,
   },
   {
-    title: "rmtDev",
+    title: "myITS StudentConnect",
     description:
-      "Job board for remote developer jobs. I was the front-end developer. It has features like filtering, sorting and pagination.",
-    tags: ["React", "TypeScript", "Next.js", "Tailwind", "Redux"],
+    "A service that manages student extracurricular credit. I merged it with an in-campus organization tracking service.",
+    tags: [ "Next.js", "Tailwind", "TypeScript", "Golang"],
     imageUrl: rmtdevImg,
-  },
-  {
-    title: "Word Analytics",
-    description:
-      "A public web app for quick analytics on text. It shows word count, character count and social media post limits.",
-    tags: ["React", "Next.js", "SQL", "Tailwind", "Framer"],
-    imageUrl: wordanalyticsImg,
   },
 ] as const;
 
