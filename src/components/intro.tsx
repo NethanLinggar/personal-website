651ab65def68db6d9b6b91e77c9608b5834282a1
Nethan Linggar <nethan.linggar@gmail.com>
2024-08-14 23:29:52 +0700

    dark mode support

--- src/components/intro.tsx
+++ src/components/intro.tsx
@@ -78,14 +78,14 @@ export default function Intro() {
           <HiDownload className="opacity-70 pb-0.2 group-hover:translate-y-0.5 transition" />
         </a>
         <a
-          className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer border-black/10"
+          className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer border-black/10 dark:bg-white/10 dark:text-white/60"
           href="https://www.linkedin.com/in/nethaneel-patricio-linggar/"
           target="_blank"
         >
           <BsLinkedin />
         </a>
         <a
-          className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer border-black/10"
+          className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer border-black/10 dark:bg-white/10 dark:text-white/60"
           href="https://github.com/NethanLinggar"
           target="_blank"
         >
